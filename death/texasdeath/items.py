dce67a4b0acddcbd4679bd8922e5b58e698f7b34
Bernard Lee <bernard.hex@gmail.com>
2016-05-26 09:11:34 +0800

    Add files via upload

---
+++ death/texasdeath/items.py
@@ -0,0 +1,46 @@
+from scrapy.item import Item, Field
+from scrapy.loader.processors import TakeFirst, MapCompose, Join
+ 
+
+class DeathItem(Item):
+ 
+    firstName = Field(
+        input_processor=MapCompose(
+            unicode.strip),
+        output_processor=Join())
+    lastName = Field(
+        input_processor=MapCompose(
+            unicode.strip),
+        output_processor=Join())
+    Age = Field(
+        input_processor=MapCompose(
+            unicode.strip),
+        output_processor=Join())
+    Date = Field(
+        input_processor=MapCompose(
+            unicode.strip),
+        output_processor=Join())
+    Race = Field(
+        input_processor=MapCompose(
+            unicode.strip),
+        output_processor=Join())
+    County = Field(
+        input_processor=MapCompose(
+            unicode.strip),
+        output_processor=Join())
+    Message = Field(
+        input_processor=MapCompose(
+            unicode.strip),
+        output_processor=Join())
+    Description = Field(
+        input_processor=MapCompose(
+            unicode.strip),
+        output_processor=Join())
+    Mid = Field(
+        input_processor=MapCompose(
+            unicode.strip),
+        output_processor=Join())
+    Education = Field(
+        input_processor=MapCompose(
+            unicode.strip),
+        output_processor=Join())
---
+++ death/texasdeath/spiders/death_spider.py
@@ -0,0 +1,62 @@
+from urlparse import urljoin
+import scrapy
+from scrapy.loader import ItemLoader
+from texasdeath.items import DeathItem
+ 
+ 
+class DeathSpider(scrapy.Spider):
+    name = "death"
+    allowed_domains = ["tdcj.state.tx.us"]
+    start_urls = [
+        "https://www.tdcj.state.tx.us/death_row/dr_executed_offenders.html"
+    ]
+    def parse(self, response):
+        sites = response.xpath('//table/tbody/tr')
+        for site in sites:
+            item = DeathItem()
+            loader = ItemLoader(item,selector=site)
+            loader.add_xpath('Mid','td[1]/text()')
+            loader.add_xpath('firstName','td[5]/text()')
+            loader.add_xpath('lastName','td[4]/text()')
+            loader.add_xpath('Date','td[8]/text()')
+            loader.add_xpath('Race','td[9]/text()')
+            loader.add_xpath('County','td[10]/text()')
+            loader.add_xpath('Age','td[7]/text()')
+            
+            url = urljoin(response.url, site.xpath("td[2]/a/@href").extract_first())
+            urlLast = urljoin(response.url, site.xpath("td[3]/a/@href").extract_first())
+ 
+            if url.endswith(("jpg","no_info_available.html")):
+                loader.add_value('Description',u'')
+                if urlLast.endswith("no_last_statement.html"):
+                    loader.add_value('Message',u'No last statement')
+                    yield loader.load_item()
+                else:
+                    request = scrapy.Request(urlLast, meta={"item" : loader.load_item()}, callback =self.parse_details2)
+                    yield request
+            else:        
+                request = scrapy.Request(url, meta={"item": loader.load_item(),"urlLast" : urlLast}, callback=self.parse_details)
+                yield request
+ 
+    def parse_details(self, response):
+
+        item = response.meta["item"]
+        urlLast = response.meta["urlLast"]
+
+        loader = ItemLoader(item,response=response)
+        loader.add_xpath("Description","//*[@id='body']/p[3]/text()")
+        loader.add_xpath("Education","//td[. = 'Education Level (Highest Grade Completed)']/following-sibling::td[1]/text()")
+
+        if urlLast.endswith("no_last_statement.html"):
+            loader.add_value('Message',u'')
+            return loader.load_item()
+        else:
+            request = scrapy.Request(urlLast, meta={"item": loader.load_item()}, callback=self.parse_details2)
+            return request
+ 
+    def parse_details2(self, response):
+        item = response.meta["item"]
+        loader = ItemLoader(item,response=response)
+        loader.add_xpath("Message","//*[contains(., 'Last Statement:')]/following-sibling::p/text()")
+        loader.add_xpath("Message","//*[contains(., 'Last Statement:')]/following-sibling::p/span/text()")
+        return loader.load_item()
